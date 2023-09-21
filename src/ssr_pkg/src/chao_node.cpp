2fec748dd72e15d2a0d10c6ef261ef216952352c
JinHeMu <490754775@qq.com>
2023-09-22 00:44:22 +0800

    年轻人的第一个SLAM

--- src/ssr_pkg/src/chao_node.cpp
+++ src/ssr_pkg/src/chao_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <qq_msgs/Carry.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,14 +8,16 @@ int main(int argc, char *argv[])
     printf("我是个马超\n");
 
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::String>("kuai_shang_che", 10);
+    ros::Publisher pub = nh.advertise<qq_msgs::Carry>("kuai_shang_che", 10);
 
     ros::Rate loop_rate(10);
 
     while (ros::ok())
     {
         printf("我要开始刷屏了\n");
-        std_msgs::String msg;
+        qq_msgs::Carry msg;
+        msg.grade = "荣耀王者";
+        msg.star  = 1000;
         msg.data = "国父马超带你们飞";
         pub.publish(msg);
         loop_rate.sleep();
