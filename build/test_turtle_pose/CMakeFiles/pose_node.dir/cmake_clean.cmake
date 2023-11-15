9b8a24ac8f715697c6a6df4aa977cc8a22beb219
JinHeMu <490754775@qq.com>
2023-11-15 16:33:07 +0800

    11/14

---
+++ build/test_turtle_pose/CMakeFiles/pose_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_node.dir/src/pose_node.cpp.o"
+  "/home/a/my_ros/devel/lib/test_turtle_pose/pose_node.pdb"
+  "/home/a/my_ros/devel/lib/test_turtle_pose/pose_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test_turtle_pose/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/a/my_ros/src/test_turtle_pose
+# Build directory: /home/a/my_ros/build/test_turtle_pose
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
